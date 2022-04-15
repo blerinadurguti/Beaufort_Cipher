4a73d4c13a3add269244f14cd55135de463367de
blertaujkani <94452970+blertaujkani@users.noreply.github.com>
2022-04-15 18:25:13 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "beaufort.h"
+
+int main(){
+    string text, key, out;
+    cout<<"Text: ";
+    cin>>text;
+    cout<<"Key: ";
+    cin>>key;
+    out=beaufort(text, key);
+    cout<<out<<"\n";
+    
+}
